fb9ed09974d857031603e129473152ac5b6d0748
Dhruvil <kotharidhruvil3@gmail.com>
2021-09-16 14:26:07 +0530

    wdcs

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -1,9 +1,13 @@
 import React from "react";
+import UserNav from "../../components/Nav/UserNav";
 
 function History() {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
         <div className="col">user History Page</div>
       </div>
     </div>
