8b454b10e980ac343380e004cd5a0b921fd92e57
Dhruvil <kotharidhruvil3@gmail.com>
2021-09-17 14:41:51 +0530

    wdcs

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -1,14 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/Nav/AdminNav";
-
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  createCategory,
+  updateCategory,
+  getCategories,
+  getCategory,
+  removeCategory,
+} from "../../../functions/category";
 function CategoryCreate() {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () => {
+    getCategories()
+      .then((c) => {
+        setCategories(c.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // console.log(name);
+    createCategory({ name }, user.token)
+      .then((res) => {
+        // console.log(res);
+        setLoading(false);
+
+        setName("");
+        toast.success(`${res.data.name} created`);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.status === 400) {
+          toast.error(err.response.data);
+          // console.error(err);
+        }
+      });
+  };
+
+  const categoryForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            required
+            autoFocus
+            type="text"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <button type="submit" className="btn  btn-outline-primary">
+            {loading ? "Loading ....." : "Save"}
+          </button>
+        </div>
+      </form>
+    );
+  };
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category Create Page</div>
+        <div className="col">
+          <h4>Create Category</h4>
+          {categoryForm()}
+        </div>
       </div>
     </div>
   );
